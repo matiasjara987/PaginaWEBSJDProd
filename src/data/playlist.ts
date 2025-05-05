8930a185f110b10b4ec731194f6d06ec52f64b6c
Matias jara <matiasjats@gmail.com>
2025-05-05 03:29:52 -0400

    Inicio de nueva funcionalidad

---
+++ src/data/playlist.ts
@@ -0,0 +1,20 @@
+export const playlist = [
+  {
+    id: "dQw4w9WgXcQ",
+    title: "Never Gonna Give You Up",
+    artist: "Rick Astley",
+    thumbnail: "https://img.youtube.com/vi/dQw4w9WgXcQ/mqdefault.jpg"
+  },
+  {
+    id: "9bZkp7q19f0",
+    title: "Gangnam Style",
+    artist: "PSY",
+    thumbnail: "https://img.youtube.com/vi/9bZkp7q19f0/mqdefault.jpg"
+  },
+  {
+    id: "kJQP7kiw5Fk",
+    title: "Despacito",
+    artist: "Luis Fonsi ft. Daddy Yankee",
+    thumbnail: "https://img.youtube.com/vi/kJQP7kiw5Fk/mqdefault.jpg"
+  }
+]; 
---
+++ src/data/slides.ts
@@ -0,0 +1,14 @@
+export const slides = [
+  {
+    title: "Bienvenida",
+    content: "Mensaje de bienvenida"
+  },
+  {
+    title: "Ubicación",
+    content: "Información de ubicación"
+  },
+  {
+    title: "Música",
+    content: "Reproductor de música"
+  }
+]; 
