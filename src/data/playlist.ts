3d337ceff1ff0699e43553cf36da87fdcb8639c8
Matias jara <matiasjats@gmail.com>
2025-05-05 10:38:43 -0400

    nuevas canciones

--- src/data/playlist.ts
+++ src/data/playlist.ts
@@ -16,5 +16,35 @@ export const playlist = [
     title: "Clouds Know Me ",
     artist: "MusicaTerapia",
     thumbnail: "https://img.youtube.com/vi/XPrubsEfA6A/mqdefault.jpg"
-  }
+  },
+  {
+      "id": "OKHSQveDPMk",
+      "title": "Kenneth",
+      "artist": "OCB Relax",
+      "thumbnail": "https://img.youtube.com/vi/OKHSQveDPMk/mqdefault.jpg"
+    },
+    {
+      "id": "fNh2yB0w8gU",
+      "title": "Deep Relaxing Music",
+      "artist": "Soothing Relaxation",
+      "thumbnail": "https://img.youtube.com/vi/fNh2yB0w8gU/mqdefault.jpg"
+    },
+    {
+      "id": "WxN3tkPkAvw",
+      "title": "Peaceful & Inspiring Ambient Music",
+      "artist": "Art Vlog",
+      "thumbnail": "https://img.youtube.com/vi/WxN3tkPkAvw/mqdefault.jpg"
+    },
+    {
+      "id": "NwSSSLl0q24",
+      "title": "Almost Time (Instrumental)",
+      "artist": "Just Instrumental Music",
+      "thumbnail": "https://img.youtube.com/vi/NwSSSLl0q24/mqdefault.jpg"
+    },
+    {
+      "id": "OhKBwCsCLjw",
+      "title": "Gentle Ambient Music for Relaxation and Mindfulness",
+      "artist": "Unknown",
+      "thumbnail": "https://img.youtube.com/vi/OhKBwCsCLjw/mqdefault.jpg"
+    } 
 ]; 
